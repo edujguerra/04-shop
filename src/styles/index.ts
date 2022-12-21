d9c003f156c463c7efd6ab2319ffcab6aed7f60f
Eduardo J. Guerra <edujguerra@gmail.com>
2022-12-21 13:01:13 -0300

    2212211301

---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+    config,
+    styled,
+    globalCss,
+    css,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme
+} = createStitches({
+    theme: {
+        colors: {
+            rocketsheat: '#8257e6'
+        }
+    }
+})
+
