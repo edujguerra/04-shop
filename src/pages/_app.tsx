ca292c15cfbce6b63131d9d6056171d88ad61ae7
Eduardo J. Guerra <edujguerra@gmail.com>
2022-12-21 21:40:23 -0300

    2212212140

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
-import logoImg from "../assets/logo.svg"
 import { Container, Header } from "../styles/pages/app";
+import logoImg from "../assets/logo.svg"
+import Image from 'next/image'
+
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg}  alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>    
